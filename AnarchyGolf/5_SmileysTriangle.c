fb01d2db5f05d21c8c37e54d04a9a9225d3f254f
YoshiokaTakuma <amkaut.develop@gmail.com>
2018-09-23 16:52:19 +0900

    size=92

--- AnarchyGolf/5_SmileysTriangle.c
+++ AnarchyGolf/5_SmileysTriangle.c
@@ -1,1 +1,1 @@
-void main(i,j){for(puts(":");i<34;++i){printf(":-");for(j=0;j<2*i-3;++j){printf(")");}puts("");}}
+void main(i,j){for(puts(":");i<34;++i,puts(""))for(j=printf(":-");j<2*i-1;++j)printf(")");}
